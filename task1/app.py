60f63f82b7581ab000670290b904ba7d29744eab
Seeratul <felix.senkbei@gmail.com>
2024-11-26 19:03:39 +0100

    no cheat
    ChatGPTdidn´t cut it we are coding now

--- task1/app.py
+++ task1/app.py
@@ -1,5 +1,18 @@
 import streamlit as st
+import random as rand
 
-st.title("Hello world")
+st.title("Guess the Number between 1 and 100")
 
-st.write("This is my first web app.")
+st.write("Guess the Number between 1 and 100")
+
+secret_number = rand.choice(range(101))
+
+st.write("Pssst the Number is ")
+st.write(secret_number)
+user_guess = st.number_input("Pick a number between 1 and 100")
+
+if st.button("Guess?"):
+    if user_guess == secret_number:
+        print("Congrats you did it!")
+else:
+    st.write("Shuttingdown")
